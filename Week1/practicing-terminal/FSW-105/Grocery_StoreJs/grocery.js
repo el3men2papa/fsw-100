29d446a0f06df417e0748f0bc4f150b9e30e8f8b
el3men2papa <65041621+el3men2papa@users.noreply.github.com>
2020-07-07 22:24:46 -0400

    fisrt commit

--- Week1/practicing-terminal/FSW-105/Grocery_StoreJs/grocery.js
+++ Week1/practicing-terminal/FSW-105/Grocery_StoreJs/grocery.js
@@ -3,11 +3,12 @@
 var shopper = {
     groceryCart: ["Pizza", "Meat", "Milk", "Cheeese","Oil" ],
     isOnStock: true,
-    prices: 5.56,
-    price: function() {
-      return this.groceryCart [0] + " $" + this.prices;
+    isle: "refigerator section", /* I added strings seperate, my string in the arrays did not qualify as strings for the grades */
+    price: 5.56,
+    cart: function() {
+      return this.groceryCart [0] + " $" + this.price;
     }
   }; 
 
-  console.log(shopper.price())
+  console.log(shopper.cart())
 
